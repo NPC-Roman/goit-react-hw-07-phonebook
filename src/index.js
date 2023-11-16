446043fd190f3f4f16088116944960b1c8c1acd7
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-11-16 13:53:07 +0200

    change07

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
@@ -14,6 +14,21 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from 'components/App';
+import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>
 );
